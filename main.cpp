cba03fb6e48a58acc9fb636da027c502a9a777cb
Andrey Dubrovin <verssy236@gmail.com>
2024-04-08 22:52:19 +0700

    Implemented simple RAII timer logic

--- main.cpp
+++ main.cpp
@@ -1,11 +1,28 @@
+#include <thread>
 #include <iostream>
 
 #include "omp_timer.h"
 
+void work()
+{
+    using namespace std::chrono_literals;
+    std::this_thread::sleep_for(1s);
+}
+
 int main()
 {
-    TIMER;
+    TIMER(main);
+    work();
+
     for (int i = 0; i < 5; i++) {
-        INNER_TIMER;
+        INNER_TIMER(for);
+        work();
+
+        if (i == 1) {
+            INNER_TIMER(if);
+            work();
+        }
     }
+
+    work();
 }
--- omp_timer.h
+++ omp_timer.h
@@ -1,37 +1,61 @@
 #ifndef OMP_TIMER_H
 #define OMP_TIMER_H
 
+#include <chrono>
 #include <iostream>
+#include <string>
 
-struct OmpTimer {
-    static inline int64_t timerDepth = 0;
-
-    explicit OmpTimer(const bool isInner) : isInner(isInner)
+class OmpTimer
+{
+public:
+    OmpTimer(const std::string name, const bool isInner)
+        : name(name), isInner(isInner), start(clock::now())
     {
         if (isInner) {
             timerDepth++;
         }
 
-        printf("Timer[inner = %d, depth = %d] START\n", isInner, timerDepth);
+        printf("|%s %s [START]\n", std::string(2 * timerDepth, '-').c_str(), name.c_str());
     }
 
     ~OmpTimer()
     {
-        printf("Timer[inner = %d, depth = %d] END\n", isInner, timerDepth);
-
+        printf("|%s %s [END] = %lfs\n", std::string(2 * timerDepth, '-').c_str(), name.c_str(),
+            durca<std::chrono::nanoseconds>());
         if (isInner) {
             timerDepth--;
         }
     }
 
 private:
+    template<typename DurationUnit>
+    double durca()
+    {
+        using namespace std::chrono;
+
+        constexpr double multiplier =
+            std::is_same_v<std::chrono::seconds, DurationUnit> ? 1
+            : std::is_same_v<std::chrono::milliseconds, DurationUnit> ? 1e-3
+            : std::is_same_v<std::chrono::microseconds, DurationUnit> ? 1e-6
+            : 1e-9;
+
+        return duration_cast<DurationUnit>(clock::now() - start).count() * multiplier;
+    }
+
+private:
+    using clock = std::chrono::high_resolution_clock;
+
+    const std::string name;
     const bool isInner;
+    const clock::time_point start;
+
+    static inline int64_t timerDepth = 0;
 };
 
-#define CONCAT2(a, b) a ## b
+#define CONCAT2(a, b) a##b
 #define CONCAT(a, b) CONCAT2(a, b)
-#define CREATE_TIMER(name, boolVal) name(boolVal)
-#define TIMER OmpTimer CREATE_TIMER(CONCAT(__local_timer_, __LINE__), false)
-#define INNER_TIMER OmpTimer CREATE_TIMER(CONCAT(__local_timer_, __LINE__), true)
+#define CREATE_TIMER(timerVariableName, timerName, isInner) timerVariableName(timerName, isInner)
+#define TIMER(name) OmpTimer CREATE_TIMER(CONCAT(__local_timer_, __LINE__), #name, false)
+#define INNER_TIMER(name) OmpTimer CREATE_TIMER(CONCAT(__local_timer_, __LINE__), #name, true)
 
 #endif
